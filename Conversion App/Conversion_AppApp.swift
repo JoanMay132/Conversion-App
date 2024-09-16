70bf304995b729411059b24631eb7ac9ce97802c
JoanMay132 <jcmd270@gmail.com>
2024-09-16 11:57:33 -0600

    Initial Commit

---
+++ Conversion App/Conversion_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conversion_AppApp.swift
+//  Conversion App
+//
+//  Created by Joan May on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Conversion_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
