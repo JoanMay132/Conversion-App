fa2442835da1919c7f11ecb7290e56a0d3e51eb5
JoanMay132 <jcmd270@gmail.com>
2024-09-16 14:19:44 -0600

    [Feature] Conversion App Time

--- Conversion App/ContentView.swift
+++ Conversion App/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    // Lists of all units
+    let temperatures: [String] = ["Celsius", "Fahrenheit", "Kelvin"]
+    let lengthUnits: [String] = ["meters", "kilometers", "feet","yards","miles"]
+    let timeUnits: [String] = ["seconds", "minutes", "hours", "days"]
+    let volumeUnits: [String] = ["mililiters", "liters", "cups", "pits","gallons"]
+    
+    @State private var temperature: String = "Celsius"
+    @State private var lengthUnit: String = "meters"
+    @State private var timeUnit: String = "seconds"
+    @State private var volumeUnit: String = "liters"
+    
+    // Amount input
+    @State private var unit: Double = 0.0
+    
+    // Unit converted
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            
+            
+            Form {
+                //Time conversion: users choose seconds, minutes, hours, or days.
+                Section("Time conversion"){
+                    TextField("Unit: ", value: $unit, format: .number)
+                        .keyboardType(.numberPad)
+                    Picker("Time unit", selection: $timeUnit){
+                        ForEach(timeUnits, id:\.self) { timeUnit in
+                            Text(timeUnit)
+                        }
+                    }
+                    
+                }
+
+            // Temperature conversion: users choose Celsius, Fahrenheit, or Kelvin.
+
+                Section("Temperature Conversion"){
+                    
+                    Picker("Temperature unit: ", selection: $temperature) {
+                        ForEach(temperatures, id: \.self) { temperature in
+                            Text(temperature)
+                        }
+                    }
+                }
+                // Length conversion: users choose meters, kilometers, feet, yards, miles.
+                Section("Length conversion") {
+                    Picker("Length Unit: ",selection: $lengthUnit) {
+                        ForEach(lengthUnits, id: \.self) { lengthUnit in
+                            Text(lengthUnit)
+                        }
+                    }
+                }
+                
+                
+                // Volume conversion: users choose milliliters, liters, cups, pints, or gallons.
+                Section("Volume conversion"){
+                    Picker("Volume unit", selection: $volumeUnit){
+                        ForEach(volumeUnits, id:\.self) { volumeUnit in
+                            Text(volumeUnit)
+                        }
+                    }
+                }
+            }
+      
+            .navigationTitle("Convert your Units")
         }
-        .padding()
+    
     }
 }
 
